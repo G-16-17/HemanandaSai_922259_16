2b821dde2b8cdabf217c8574cdcf260d48c3b23c
SaiHemananda <naidusimhachalam2000@gmail.com>
2021-09-21 23:01:00 +0530

    Spring rest handson

--- AWS/Day4 HandsOn/Spring REST with RDS Backend/springrestdemo/src/main/java/com/learn/springrestdemo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learn.springrestdemo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.learn.springrestdemo.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
